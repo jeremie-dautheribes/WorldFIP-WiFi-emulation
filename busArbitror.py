6993dd484fcefa7c4d195540faa9cd201f3a3064
Victor333Huesca <Victor333Huesca@hotmail.com>
2019-11-10 11:09:42 +0100

    busArbitror.py: add 1st version of the arbitror
    This 1st version does the job of the FIP arbitror but instead of sending
    messages to the bus, it prints the message to the console (to be modified when
    the bus would be implemented).
    
    Signed-off-by: Victor333Huesca <Victor333Huesca@hotmail.com>

--- busArbitror.py
+++ busArbitror.py
@@ -1,1 +1,49 @@
 #!/usr/bin/env python3
+from time import sleep
+from itertools import cycle
+from frame import ID_Dat
+
+
+class BusArbitror(object):
+    '''
+    Représente un arbitre de bus du protocol World-FIP
+    '''
+    def __init__(self, table={}):
+        self._table = table
+        self._microcycle, self._macrocycle = self.cycles_from_table(table)
+
+    def do_loop(self):
+        '''
+        Loop over all messages
+
+        This 1st version simply print the messages, tgit he next version should
+        actually send the frame to the network.
+        '''
+        t2 = None
+        for t, msg in cycle(self.list_macrocycle()):
+            if t != t2:
+                t2 = t
+                sleep(bus._microcycle / 1000)
+                print(f't = {t}ms')
+
+            # Do the job
+            print('\t', msg.__dict__)
+
+    def list_macrocycle(self):
+        '''
+        Do a full macrocycle
+
+        return a tuple `(time, Frame)`
+        '''
+        for t in range(0, self._macrocycle, self._microcycle):
+            # Loop over the table to see what message should be send
+            for id, period in self._table.items():
+                if t % period == 0:
+                    yield (t, ID_Dat(id))
+
+    @staticmethod
+    def cycles_from_table(table):
+        return (100, 1000)
+        for i, p in table.items():
+            pass
+
